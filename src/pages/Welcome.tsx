6016ddea99b84fae2e4a14f77c71bbeb4c3e1ea8
Rahul Pal <usefulltech1819@gmail.com>
2026-01-24 23:06:38 +0530

    Add Welcome page

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,11 @@
+import HeroSection from '../components/HeroSection'
+
+const Welcome = () => {
+  return (
+    <div className="bg-gray-900 min-h-screen text-center">
+        <HeroSection />
+    </div>
+  )
+}
+
+export default Welcome;
