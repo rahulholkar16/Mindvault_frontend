e18bdd71ab79cbc785cbb9e3ec4cc5892e08b8ea
Rahul Pal <usefulltech1819@gmail.com>
2026-01-24 23:04:25 +0530

    Add Navbar component and add some router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router";
+import Welcome from "./pages/Welcome";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<div> Hello World </div>} />
-      <Route path="/login" element={<div> Hello Login </div>} />
-    </Routes>
-  )
+      <>
+          <Navbar />
+          <Routes>
+              <Route path="/" element={<Welcome />} />
+              <Route path="/login" element={<div> Hello Login </div>} />
+          </Routes>
+      </>
+  );
 }
 
 export default App;
