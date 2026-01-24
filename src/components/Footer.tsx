2eaf7aebbefc4dcfbe93144012254640fbab8105
Rahul Pal <usefulltech1819@gmail.com>
2026-01-25 00:23:30 +0530

    Add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+      <div className="border-t border-slate-700/50 bg-gray-900">
+          <div className="max-w-6xl mx-auto px-6 py-12 text-center text-slate-400">
+              <p>Join thousands of people building their second brain</p>
+          </div>
+      </div>
+  );
+}
+
+export default Footer;
