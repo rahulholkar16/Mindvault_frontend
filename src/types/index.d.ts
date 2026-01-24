ea86f9aa89dfb94cd96ef70ada2785714055fc9c
Rahul Pal <usefulltech1819@gmail.com>
2026-01-24 23:03:01 +0530

    Add button prop Type

---
+++ src/types/index.d.ts
@@ -0,0 +1,10 @@
+import type { ReactNode } from "react";
+
+interface ButtonProp {
+    text: string;
+    color: "primary" | "secondary" | "danger" | "success";
+    size: "small" | "large";
+    onClick?: () => void;
+    disabled: boolean;
+    children?: ReactNode;
+}
