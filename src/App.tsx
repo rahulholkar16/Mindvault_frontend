98dbd6914b38ead05cd35f8e18090f5be7a872a4
Rahul Pal <usefulltech1819@gmail.com>
2026-01-25 21:58:18 +0530

    Rearrage Router and Add Dasboard Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { Route, Routes } from "react-router";
 import Welcome from "./pages/Welcome";
-import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Singup from "./pages/Singup";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
-  return (
-      <>
-          <Navbar />
-          <Routes>
-              <Route path="/" element={<Welcome />} />
-              <Route path="/login" element={<div> Hello Login </div>} />
-          </Routes>
-      </>
-  );
-}
+    return (
+        <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Singup />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+    );
+};
 
 export default App;
