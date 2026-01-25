93637c6a76ab3c25b31ca93c49b073ef87d4649b
Rahul Pal <usefulltech1819@gmail.com>
2026-01-26 00:24:18 +0530

    Add Sidebar Item Data

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,4 +1,4 @@
-import {Zap, Shield, Share2} from 'lucide-react'
+import {Zap, Shield, Share2, Bird, Youtube, BookText, Link, Hash } from 'lucide-react'
 
 export const features = [
     {
@@ -16,4 +16,27 @@ export const features = [
         description: "Sync across all your devices, anytime, anywhere.",
         icon: <Share2 size={ 32 } className = "text-cyan-400 mb-4" />
     }
+];
+
+export const Sidebar = [
+    {
+        title: "Tweets",
+        icon: <Bird size={24} />,
+    },
+    {
+        title: "Videos",
+        icon: <Youtube size={24} />,
+    },
+    {
+        title: "Documents",
+        icon: <BookText size={24} />,
+    },
+    {
+        title: "Links",
+        icon: <Link size={24} />,
+    },
+    {
+        title: "Tags",
+        icon: <Hash size={24} />,
+    },
 ];
