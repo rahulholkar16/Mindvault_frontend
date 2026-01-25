b8b94d3cbddcf4b0f0fe97b07c4e139be39250a4
Rahul Pal <usefulltech1819@gmail.com>
2026-01-25 22:00:30 +0530

    Add Auth store

---
+++ src/store/authStore/useAuth.ts
@@ -0,0 +1,65 @@
+import { create } from "zustand";
+import type { AuthState } from "../../types";
+import axios from "axios";
+
+export const useAuth = create<AuthState>((set) => ({
+    user: null,
+    loading: false,
+    error: null,
+    isAuthenticated: false,
+    success: false,
+    
+    login: async (email, password) => {
+        try {
+            set({loading: true, error: null});
+
+            const res = await axios.post("http://localhost:3000/api/v1/auth/login", 
+                {email, password},
+                {withCredentials: true}
+            );
+
+            set({
+                user: res.data,
+                isAuthenticated: true,
+                loading: false,
+            });
+        } catch (error: any) {
+            set({
+                error: error.response?.data?.message, 
+                loading: false
+            })
+        }
+    },
+
+    logout: async () => {
+        await axios.delete("http://localhost:3000/api/v1/auth/logout", { withCredentials: true });
+        set({
+            user: null,
+            isAuthenticated: false
+        })
+    },
+
+    register: async (email, password, name) => {
+        try {
+            set({
+                loading: true,
+                error: null
+            });
+            await axios.post("http://localhost:3000/api/v1/auth/register",
+                { name, email, password },
+                { withCredentials: true }
+            );
+            set({
+                loading: false,
+                success: true
+            })
+            return true;
+        } catch (error: any) {
+            set({
+                error: error.response?.data?.message,
+                loading: false
+            })
+            return false;
+        }
+    }
+}))
