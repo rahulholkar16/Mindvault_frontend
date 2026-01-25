3bd503b40e4c9e574e7ee3c4da6f888c4c062dd6
Rahul Pal <usefulltech1819@gmail.com>
2026-01-25 23:24:39 +0530

    Add Navbar and  Footer

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,11 +1,17 @@
+import FeaturesSection from '../components/FeaturesSection';
+import Footer from '../components/Footer';
 import HeroSection from '../components/HeroSection'
+import Navbar from '../components/Navbar';
 
 const Welcome = () => {
   return (
-    <div className="bg-gray-900 min-h-screen text-center">
-        <HeroSection />
-    </div>
-  )
+      <div className="bg-gray-900 min-h-screen text-center mx-auto">
+          <Navbar />
+          <HeroSection />
+          <FeaturesSection />
+          <Footer />
+      </div>
+  );
 }
 
 export default Welcome;
