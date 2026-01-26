a67368a802c110ae3c0731dd78d25fd50c035cf8
Rahul Pal <usefulltech1819@gmail.com>
2026-01-26 22:51:58 +0530

    Add Invalid tweet componets

---
+++ src/components/InvalidTweet.tsx
@@ -0,0 +1,9 @@
+function InvalidTweet() {
+    return (
+        <div className="p-4 rounded-lg bg-red-900/20 text-red-400 text-sm">
+            Invalid tweet link
+        </div>
+    );
+}
+
+export default InvalidTweet;
