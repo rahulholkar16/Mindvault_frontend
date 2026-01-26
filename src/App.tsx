13f08ec900639762f912bbc13fcf1934c5c5b5dd
Rahul Pal <usefulltech1819@gmail.com>
2026-01-26 22:55:30 +0530

    Add twitter widgets

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,18 @@ import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
 import Singup from "./pages/Singup";
 import Dashboard from "./pages/Dashboard";
+import { useEffect } from "react";
 
 const App = () => {
+    useEffect(() => {
+        if (!window.twttr) {
+            const script = document.createElement("script");
+            script.src = "https://platform.twitter.com/widgets.js";
+            script.async = true;
+            document.body.appendChild(script);
+        }
+    }, []);
+
     return (
         <Routes>
             <Route path="/" element={<Welcome />} />
