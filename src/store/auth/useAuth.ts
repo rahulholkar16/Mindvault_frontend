4d5b56c2e889f32336c3399c2d2403c102e20d50
Rahul Pal <usefulltech1819@gmail.com>
2026-01-27 20:54:59 +0530

    Add persist middleware and format

--- src/store/auth/useAuth.ts
+++ src/store/auth/useAuth.ts
@@ -1,66 +1,82 @@
 import { create } from "zustand";
 import type { AuthState } from "../../types";
 import axios from "axios";
+import { persist } from "zustand/middleware";
 
-export const useAuth = create<AuthState>((set) => ({
-    user: null,
-    loading: false,
-    error: null,
-    isAuthenticated: false,
-    success: false,
-    
-    login: async (email, password) => {
-        try {
-            set({loading: true, error: null});
-            const res = await axios.post("http://localhost:3000/api/v1/auth/login", 
-                {email, password},
-                {withCredentials: true}
-            );
-
-            set({
-                user: res.data,
-                isAuthenticated: true,
-                loading: false,
-            });
-            return true;
-        } catch (error: any) {
-            set({
-                error: error.response?.data?.message, 
-                loading: false
-            })
-            return false;
-        }
-    },
-
-    logout: async () => {
-        await axios.delete("http://localhost:3000/api/v1/auth/logout", { withCredentials: true });
-        set({
+export const useAuth = create<AuthState>()(
+    persist(
+        (set) => ({
             user: null,
-            isAuthenticated: false
-        })
-    },
+            loading: false,
+            error: null,
+            isAuthenticated: false,
+            success: false,
 
-    register: async (email, password, name) => {
-        try {
-            set({
-                loading: true,
-                error: null
-            });
-            await axios.post("http://localhost:3000/api/v1/auth/register",
-                { name, email, password },
-                { withCredentials: true }
-            );
-            set({
-                loading: false,
-                success: true
-            })
-            return true;
-        } catch (error: any) {
-            set({
-                error: error.response?.data?.message,
-                loading: false
-            })
-            return false;
-        }
-    }
-}))
+            login: async (email, password) => {
+                try {
+                    set({ loading: true, error: null });
+                    const res = await axios.post(
+                        "http://localhost:3000/api/v1/auth/login",
+                        { email, password },
+                        { withCredentials: true },
+                    );
+
+                    set({
+                        user: res.data,
+                        isAuthenticated: true,
+                        loading: false,
+                    });
+                    return true;
+                } catch (error: any) {
+                    set({
+                        error: error.response?.data?.message,
+                        loading: false,
+                    });
+                    return false;
+                }
+            },
+
+            logout: async () => {
+                await axios.delete("http://localhost:3000/api/v1/auth/logout", {
+                    withCredentials: true,
+                });
+                set({
+                    user: null,
+                    isAuthenticated: false,
+                });
+            },
+
+            register: async (email, password, name) => {
+                try {
+                    set({
+                        loading: true,
+                        error: null,
+                    });
+                    await axios.post(
+                        "http://localhost:3000/api/v1/auth/register",
+                        { name, email, password },
+                        { withCredentials: true },
+                    );
+                    set({
+                        loading: false,
+                        success: true,
+                    });
+                    return true;
+                } catch (error: any) {
+                    set({
+                        error: error.response?.data?.message,
+                        loading: false,
+                    });
+                    return false;
+                }
+            },
+        }),
+        {
+            name: "auth-storage",
+            partialize: (s) => ({
+                user: s.user,
+                isAuthenticated: s.isAuthenticated,
+            }),
+        },
+    ),
+);
