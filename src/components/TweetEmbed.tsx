48506c1d0179a2219e18fe5d3e7070d534621190
Rahul Pal <usefulltech1819@gmail.com>
2026-01-28 23:16:43 +0530

    Add Twitter widgets api and Add line clamp in paragraph

--- src/components/TweetEmbed.tsx
+++ src/components/TweetEmbed.tsx
@@ -2,7 +2,12 @@ import { useEffect, useRef } from "react";
 import { normalizeTweetUrl } from "../utils/normalizeTweetUrl.js";
 import InvalidTweet from "./InvalidTweet";
 
-export function TweetEmbed({ url }: { url: string }) {
+export function TweetEmbed({
+    url,
+    description,
+}: {
+    url: string | undefined;
+    description: string | undefined}) {
     const ref = useRef<HTMLDivElement>(null);
 
     const data = normalizeTweetUrl(url);
@@ -12,23 +17,23 @@ export function TweetEmbed({ url }: { url: string }) {
 
         ref.current.innerHTML = "";
 
-        window.twttr.widgets
-            .createTweet(data.tweetId, ref.current, {
-                theme: "dark",
-                conversation: "none",
-                align: "center",
-            })
+        window.twttr.widgets.createTweet(data.tweetId, ref.current, {
+            theme: "dark",
+            conversation: "none",
+            align: "center",
+        });
     }, [data?.tweetId, data]);
 
     if (!data) return <InvalidTweet />;
 
     return (
         <div className="relative my-4 rounded-lg overflow-hidden">
-
             <div
                 ref={ref}
-                className={`max-h-[240px] overflow-y-auto no-scrollbar`}
+                className={`max-h-[200px] overflow-y-auto no-scrollbar rounded-lg`}
             />
+
+            <p className="m-2 text-white line-clamp-2">{description}</p>
         </div>
     );
 }
