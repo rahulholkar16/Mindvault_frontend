a63dc374e613f38e0f698f58c409080b0f48ba54
Rahul Pal <usefulltech1819@gmail.com>
2026-01-29 16:08:03 +0530

    Add type in sidebar array

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,4 +1,4 @@
-import {Zap, Shield, Share2, Bird, Youtube, BookText, Link, Hash } from 'lucide-react'
+import {Zap, Shield, Share2, Bird, Youtube, BookText, Link, Hash, GalleryHorizontalEnd } from 'lucide-react'
 
 export const features = [
     {
@@ -20,23 +20,33 @@ export const features = [
 
 export const sidebar = [
     {
+        title: "All Post",
+        icon: <GalleryHorizontalEnd />,
+        type: "all"
+    },
+    {
         title: "Tweets",
         icon: <Bird size={20} />,
+        type: "tweet",
     },
     {
         title: "Videos",
         icon: <Youtube size={20} />,
+        type: "video",
     },
     {
         title: "Documents",
         icon: <BookText size={20} />,
+        type: "document",
     },
     {
         title: "Links",
         icon: <Link size={20} />,
+        type: "url",
     },
     {
         title: "Tags",
         icon: <Hash size={20} />,
+        type: "tag",
     },
 ];
