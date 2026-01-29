ee0250a8ad736a34d66ab539300673d03d6436cd
Rahul Pal <usefulltech1819@gmail.com>
2026-01-29 16:06:00 +0530

    Update Optimise App using lazy and suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,15 @@
-import { Route, Routes } from "react-router";
-import Welcome from "./pages/Welcome";
-import Login from "./pages/Login";
-import Singup from "./pages/Singup";
-import Dashboard from "./pages/Dashboard";
-import { useEffect } from "react";
-import { useAuth } from "./store/auth/useAuth";
-import ProtectedRoute from "./routes/ProtectedRoute";
+import { Suspense } from "react";
+import AuthGate from "./gate/AuthGate";
+import AppRoutes from "./routes/AppRoutes";
+import AppLoader from "./components/AppLoader";
 
 const App = () => {
-    const checkAuth = useAuth(state => state.checkAuth);
-    const refreshAccessToken = useAuth((state) => state.refreshAccessToken);
-    useEffect(() => {
-        const initAuth = async () => {
-            const ok = await checkAuth();
-
-            if (!ok) {
-                const refreshed = await refreshAccessToken();
-                if (refreshed) {
-                    await checkAuth(); // 👈 THIS IS MISSING
-                }
-            }
-        };
-
-        initAuth();
-    }, [checkAuth, refreshAccessToken]);
-
     return (
-        <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Singup />} />
-            
-            <Route element={<ProtectedRoute />}>
-                <Route path="/dashboard" element={<Dashboard />} />
-            </Route>
-        </Routes>
+        <AuthGate>
+            <Suspense fallback={<AppLoader />}>
+                <AppRoutes />
+            </Suspense>
+        </AuthGate>
     );
 };
 
