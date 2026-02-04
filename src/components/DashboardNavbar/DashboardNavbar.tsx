a9fad33dafd0451f107c83c743a4d4fcab1576b3
Rahul Pal <usefulltech1819@gmail.com>
2026-02-05 00:42:16 +0530

    Change FIle name

--- src/components/DashboardNavbar/DashboardNavbar.tsx
+++ src/components/DashboardNavbar/DashboardNavbar.tsx
@@ -4,10 +4,8 @@ import Button from "../Button";
 
 const DashboardNavbar = ({ setAddForm }: { setAddForm: () => void}) => {
     const ShareBrain = useAuth((s) => s.onShare);
-    const link = useAuth((s) => s.shareLink);
     const onBrainShare = async () => {
         await ShareBrain();
-        console.log(link);
     };
     return (
         <div className="min-h-10 mb-4 max-h-12 flex justify-between items-center">
