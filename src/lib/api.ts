834446e672a341de81fd3d3ab6bc1a59165e446b
Rahul Pal <usefulltech1819@gmail.com>
2026-02-08 22:57:38 +0530

    Change backend url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: "https://mindvault-blond.vercel.app/api/v1",
     withCredentials: true,
 });
